423659b307cd46bf61937ba7dcd8224f3a47c754
thabsheer <97297234+thabsheer002@users.noreply.github.com>
2022-10-20 23:00:20 +0530

    substraction
    substraction using python

---
+++ substract.py
@@ -0,0 +1,9 @@
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+substraction = float(num1) - float(num2)
+
+# Display the substraction
+print('The substraction of {0} and {1} is {2}'.format(num1, num2, substraction))
