04c6e6b7bda45557904cd857d85ec1c9fcf5e431
thabsheer <97297234+thabsheer002@users.noreply.github.com>
2022-10-20 22:52:21 +0530

    division
    division using python

---
+++ division.py
@@ -0,0 +1,12 @@
+# Store input numbers
+from __future__ import division
+
+
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+division = float(num1) / float(num2)
+
+# Display the substraction
+print('The division of {0} and {1} is {2}'.format(num1, num2, division))
